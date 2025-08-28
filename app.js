651ffe1058d2ffdd04ffd80e498532435aafc58f
Leonardo Fagundes <leogundes18@gmail.com>
2025-08-28 13:58:06 -0300

    Created Node API with express for InPages project

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express');
+const rotaLivro = require("./rotas/livro");
+
+const app = express();
+app.use(express.json());
+
+app.use("/livros", rotaLivro);
+
+const port = 8000;
+
+
+app.listen(port, () => {
+    console.log(`Server rodando na porta ${port}`);
+})
